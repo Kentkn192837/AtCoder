8a955d899bb5e92c8cd8c1c351b12bf7c4b19379
Kentkn192837 <agriculture5012@gmail.com>
2021-09-03 18:22:28 +0900

    ACではないver2

--- BeginnerContest/216/C.py
+++ BeginnerContest/216/C.py
@@ -2,25 +2,17 @@ class ManyBalls():
     log = ''
 
     def __init__(self, n):
-        self.init_n = n
         self.calc(n)
 
     def calc(self, n):
-
-        while True:
-            if n == 1:
-                return
-
+        while n >= 1:
             if n % 2 == 1:
                 self.magic_A()
                 n -= 1
-                continue
-
-            if n % 2 == 0:
+            else:
                 self.magic_B()
                 n /= 2
                 n = int(n)
-                continue
 
     def magic_A(self):
         self.log += 'A'
@@ -30,4 +22,4 @@ class ManyBalls():
 
 N = int(input())
 result = ManyBalls(N)
-print('A{}'.format(result.log[::-1]))
+print(result.log[::-1])
