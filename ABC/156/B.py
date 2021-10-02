24957bee172ec90966a97d778a2d761e307aad33
Kentkn192837 <agriculture5012@gmail.com>
2021-10-03 02:51:44 +0900

    AC

--- ABC/156/B.py
+++ ABC/156/B.py
@@ -1,13 +1,10 @@
 def n_ary(n, k):
-    remainder = []
-    while True:
-        n = int(n / k)
-        remainder.append(n % k)
-        if int(n / k) == 0:
-            remainder.append(n % k)
-            break
-    remainder = [str(x) for x in remainder]
-    return len(''.join(remainder))
+    s = ''
+    while n >= k:
+        s += str(n % k)
+        n = n // k
+    s += str(n)
+    return len(s[::-1])
 
 n, k = map(int, input().split())
 result = n_ary(n, k)
