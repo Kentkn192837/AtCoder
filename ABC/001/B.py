510a8396954da9044f7b0b4f5560ff7b44f66642
Kentkn192837 <agriculture5012@gmail.com>
2021-10-03 02:04:06 +0900

    AC

--- ABC/001/B.py
+++ ABC/001/B.py
@@ -1,13 +1,20 @@
 from math import floor
+
+def zfill(x):
+    return str(floor(x)).zfill(2)
+
+def calc(m):
+    if m < 0.1:
+        return '00'
+    elif 0.1 <= m and m <= 5:
+        return zfill(m * 10)
+    elif 6 <= m and m <= 30:
+        return zfill(m + 50)
+    elif 35 <= m and m <= 70:
+        return zfill((m - 30) / 5) + 80)
+    elif 70 <= m:
+        return 89
+
 m = int(input()) / 1000
-if m < 0.1:
-    vv = '00'
-elif 0.1 <= m and m <= 5:
-    vv = str(floor(m * 10)).zfill(2)
-elif 6 <= m and m <= 30:
-    vv = str(floor(m + 50)).zfill(2)
-elif 35 <= m and m <= 70:
-    vv = str(floor((m - 30) / 5) + 80).zfill(2)
-elif 70 <= m:
-    vv = 89
+vv = calc(m)
 print(vv)
