dd0a2a85bf2a1f79843246f05f560d40eb93f4fb
Kentkn192837 <agriculture5012@gmail.com>
2021-10-03 05:38:37 +0900

    AC

---
+++ APG4b/EX3.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << 100 * (100 + 1) / 2 << std::endl;
+}
---
+++ APG4b/EX8.py
@@ -0,0 +1,11 @@
+p = int(input())
+if p == 1:
+    price = int(input())
+    N = int(input())
+    print(price * N)
+elif p == 2:
+    text = input()
+    price = int(input())
+    N = int(input())
+    print('{}!'.format(text))
+    print(price * N)
