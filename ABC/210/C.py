48ccf8a2e0382f38dadf2e2f55c4421927d8406f
Kentkn192837 <agriculture5012@gmail.com>
2021-10-03 10:55:32 +0900

    AC

--- ABC/210/C.py
+++ ABC/210/C.py
@@ -1,17 +1,18 @@
 """
 尺取り法(差分計算)
 """
+from collections import Counter
 
 n, k = map(int, input().split())
 c = list(map(int, input().split()))
-max = 0
-for i in range(n - k + 1):
-    comp = []
-    for j in c[i:i + k]:
-        if j not in comp:
-            comp.append(j)
-    if max <= len(comp):
-        max = len(comp)
+counter = Counter(c[0:k])
+max = len(counter)
+for i in range(1, n - k + 1):
+    counter[c[i-1]] -= 1
+    counter[c[i+k-1]] += 1
+    if counter[c[i-1]] == 0:
+        del counter[c[i-1]]
+    max = len(counter) if max <= len(counter) else max
     if max == k:
         break
 print(max)
