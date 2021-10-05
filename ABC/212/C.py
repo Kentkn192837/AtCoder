953c6fd70a1d784f76438c1aa94f430ba5c31c35
Kentkn192837 <agriculture5012@gmail.com>
2021-10-06 02:04:24 +0900

    AC 二分探索

--- ABC/212/C.py
+++ ABC/212/C.py
@@ -1,14 +1,25 @@
+import bisect
+
 n, m = map(int, input().split())
 a_seaquence = list(map(int, input().split()))
 b_seaquence = list(map(int, input().split()))
 
 a_seaquence = list(set(a_seaquence))
-b_seaquence = list(set(b_seaquence))
+b_seaquence = sorted(list(set(b_seaquence)))
 
 min_candidate = []
 for i in a_seaquence:
-    candidate = []
-    for j in b_seaquence:
-        candidate.append(abs(j - i))
-    min_candidate.append(min(candidate))
+    # print('{}の処理'.format(i))
+    x_index = bisect.bisect_left(b_seaquence, i)
+    # print("x_index:", x_index)
+    if x_index == len(b_seaquence):
+        x_index -= 1
+        comp = abs(i - b_seaquence[x_index])
+    elif x_index == 0:
+        comp = abs(i - b_seaquence[x_index])
+    else:
+        comp = min([abs(i - b_seaquence[x_index - 1]), abs(i - b_seaquence[x_index])])
+    # print("comp", comp)
+    min_candidate.append(comp)
+# print("min_candidate", min_candidate)
 print(min(min_candidate))
