45533241d602e9af3fb845edfbc458a6ddc70169
Kentkn192837 <agriculture5012@gmail.com>
2021-10-06 15:14:29 +0900

    AC 二分探索

--- ABC/212/C.py
+++ ABC/212/C.py
@@ -1,24 +1,24 @@
 import bisect
 
 n, m = map(int, input().split())
-a_seaquence = list(map(int, input().split()))
-b_seaquence = list(map(int, input().split()))
+a_sequence = list(map(int, input().split()))
+b_sequence = list(map(int, input().split()))
 
-a_seaquence = list(set(a_seaquence))
-b_seaquence = sorted(list(set(b_seaquence)))
+a_sequence = list(set(a_sequence))
+b_sequence = sorted(list(set(b_sequence)))
 
 min_candidate = []
-for i in a_seaquence:
+for i in a_sequence:
     # print('{}の処理'.format(i))
-    x_index = bisect.bisect_left(b_seaquence, i)
+    x_index = bisect.bisect_left(b_sequence, i)
     # print("x_index:", x_index)
-    if x_index == len(b_seaquence):
+    if x_index == len(b_sequence):
         x_index -= 1
-        comp = abs(i - b_seaquence[x_index])
+        comp = abs(i - b_sequence[x_index])
     elif x_index == 0:
-        comp = abs(i - b_seaquence[x_index])
+        comp = abs(i - b_sequence[x_index])
     else:
-        comp = min([abs(i - b_seaquence[x_index - 1]), abs(i - b_seaquence[x_index])])
+        comp = min([abs(i - b_sequence[x_index - 1]), abs(i - b_sequence[x_index])])
     # print("comp", comp)
     min_candidate.append(comp)
 # print("min_candidate", min_candidate)
