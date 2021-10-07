09d61eba408b679f72ade642b0740a1b924fddeb
Kentkn192837 <agriculture5012@gmail.com>
2021-10-07 23:45:11 +0900

    途中

---
+++ ABC/211/C.py
@@ -0,0 +1,11 @@
+def dp(str):
+    keyword = 'chokudai'
+    
+
+def test(prm, name):
+    print('{}:'.format(name), prm)
+
+if __name__ == '__main__':
+    s = list(input())
+    MOD = 10 ** 9 + 7
+    dp = dp(s)
