8cbecef12588e1c0a30f87cab249f0c3e3ae8cd9
Kentkn192837 <agriculture5012@gmail.com>
2021-10-22 20:22:43 +0900

    Low Elements: AC

--- ABC/152/C.py
+++ ABC/152/C.py
@@ -1,21 +1,13 @@
-from copy import deepcopy
-
 def main():
     n = int(input())
     p = list(map(int, input().split()))
 
-    comp_p = sorted(deepcopy(p))
     i_count = 0
-    for i in comp_p:
-        satisfy = []
-        p_i = p.index(i)
-        for j in range(p_i, -1, -1):
-            if i <= p[j]:
-                satisfy.append(True)
-            else:
-                satisfy.append(False)
-        if False not in satisfy:
+    minimum = p[0]
+    for i in p:
+        if i <= minimum:
             i_count += 1
+        minimum = min(minimum, i)
     print(i_count)
 
 if __name__ == '__main__':
