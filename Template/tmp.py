ddc0abec72f8ba1099c9bb021bbbe20d3962b132
Kentkn192837 <agriculture5012@gmail.com>
2021-10-31 15:21:46 +0900

    テンプレート

--- Template/tmp.py
+++ Template/tmp.py
@@ -4,6 +4,15 @@ from collections import Counter, deque
 def test(variable, x):
     print('{}: {}'.format(variable, x))
 
+def input_two_array(n):
+    a = []
+    b = []
+    for _ in range(n):
+        a_i, b_i = map(int, input().split())
+        a.append(a_i)
+        b.append(b_i)
+    return a, b
+
 def main():
 
 if __name__ == '__main__':
