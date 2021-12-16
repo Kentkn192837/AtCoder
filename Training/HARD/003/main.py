c4ab6d88e85aa188f021cf948d020c935464d83b
Kentkn192837 <agriculture5012@gmail.com>
2021-12-16 23:48:29 +0900

    2nd commit

--- Training/HARD/003/main.py
+++ Training/HARD/003/main.py
@@ -1,8 +1,7 @@
 def main():
     h, w = map(int, input().split())
     S = [list(input()) for _ in range(h)]
-    print(h)
-    print(w)
+    dp = [[-1] * w for _ in range(h)]
 
 if __name__ == '__main__':
     main()
