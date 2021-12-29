5a51e3e9cb233c676fc961106b1c6388fe9afd92
Kentkn192837 <agriculture5012@gmail.com>
2021-12-29 10:46:12 +0900

    素因数分解,約数列挙を追加

--- Template/tmp.py
+++ Template/tmp.py
@@ -13,6 +13,35 @@ def input_two_array(n):
         b.append(b_i)
     return a, b
 
+# 素因数分解
+def prime_factorize(n):
+    x = []
+    while n % 2 == 0:
+        x.append(2)
+        n //= 2
+    f = 3
+    while f*f <= n:
+        if n % f == 0:
+            x.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        x.append(n)
+    return x
+
+# 約数を列挙する関数
+def make_divisors(n):
+    lower_divisors, upper_divisors = [], []
+    i = 1
+    while i*i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n//i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
+
 def main():
 
 if __name__ == '__main__':
