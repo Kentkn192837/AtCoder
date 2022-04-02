f074535aa0f2f1a2b4db06d886069f3fdf51c4f0
Kentkn192837 <agriculture5012@gmail.com>
2022-04-03 03:18:12 +0900

    素数列挙を行う関数の追加

--- Template/tmp.py
+++ Template/tmp.py
@@ -42,6 +42,21 @@ def make_divisors(n):
         i += 1
     return lower_divisors + upper_divisors[::-1]
 
+# 素数を列挙する関数(エラトステネスの篩)
+def prime_sieve(n):
+    prime = [ True ] * (n + 1)
+
+    LIMIT = int(n ** 0.5)
+    for i in range(2, LIMIT + 1):
+        if prime[i] == True:
+            for j in range(2 * i, n + 1, i):
+                prime[j] = False
+
+    # n 以下の素数を小さい順に出力
+    for i in range(2, n + 1):
+        if prime[i] == True:
+            print(i)
+
 def main():
 
 if __name__ == '__main__':
