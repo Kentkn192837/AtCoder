7201f86c5ad2daf6ed5dbf47faa6e24b759f06b2
Kentkn192837 <agriculture5012@gmail.com>
2022-04-09 00:26:56 +0900

    WA

--- Training/Medium/007/main.py
+++ Training/Medium/007/main.py
@@ -2,5 +2,15 @@ def main():
     N = int(input())
     S = input()
 
+    max_cand = 0
+    
+    for i in range(1, len(S) - 1):
+        left = list(S[:i])
+        right = list(S[i:-1])
+        matched_list = list(set(left) & set(right))
+        max_cand = max(max_cand, len(matched_list))
+    
+    print(max_cand)
+
 if __name__ == '__main__':
     main()
