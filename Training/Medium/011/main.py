bbd85674cf522f8e01f848cf0d2c82c502e3cc5d
Kentkn192837 <agriculture5012@gmail.com>
2022-05-19 00:25:09 +0900

    AC

--- Training/Medium/011/main.py
+++ Training/Medium/011/main.py
@@ -7,11 +7,20 @@ def input_two_array(n):
         b.append(b_i)
     return a, b
 
+def l1_distance(x1, x2, y1, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
+
 def main():
     N, M = map(int, input().split())
-    a, b = input_two_array(N)
-    c, d = input_two_array(M)
-    print(*a)
+    a, b = input_two_array(N)       # 学生の座標
+    c, d = input_two_array(M)       # チェックポイントの座標 
+
+    position = [0] * M
+    for i in range(N):
+        candidate = []
+        for j in range(M):
+            candidate.append(l1_distance(a[i], c[j], b[i], d[j]))
+        print(candidate.index(min(candidate)) + 1)
 
 if __name__ == '__main__':
     main()
