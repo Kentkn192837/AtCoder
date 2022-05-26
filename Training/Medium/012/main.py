aa4cfa328e0aaae6207c6f71545b7cdf8c494318
Kentkn192837 <agriculture5012@gmail.com>
2022-05-27 01:21:01 +0900

    消去のロジック

--- Training/Medium/012/main.py
+++ Training/Medium/012/main.py
@@ -1,7 +1,25 @@
+def show(A):
+    for a in A:
+        print(''.join(a))
+
 def main():
     H, W = map(int, input().split())
-    a = [list(input()) for _ in range(H)]
+    a = [input() for _ in range(H)]
     print(a)
+
+    row = []
+    col = []
+    
+    # 横のチェック
+    a = [x for x in a if '#' in x]
+    print(a)
+
+    # 縦のチェック
+    for i in range(len(a[0])):
+        for j in range(len(a)):
+            
+
+
 
 if __name__ == '__main__':
     main()
