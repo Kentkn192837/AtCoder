110086fe174493bc6e618617c0f7a54a7eebe21e
Kentkn192837 <agriculture5012@gmail.com>
2022-07-22 21:47:26 +0900

    upload

--- ABC/157/C.py
+++ ABC/157/C.py
@@ -1,19 +1,28 @@
 def main():
     n, m = map(int, input().split())
-    sc = [list(map(int, input().split())) for _ in range(m)]
+    
+    t = [-1] * n
+    for _ in range(m):
+        s, c = map(int, input().split())
+        s -= 1
+        if t[s] != -1 and t[s] != c:
+            print(-1)
+            exit()
+        t[s] = c
 
-    cand = []
-    for i in range(1000):
-        num = len(str(i))
-        if num != n:
-            continue
-        else:
-            for j in sc:
-                s, c = j:
-                if num[s - 1] != c:
-                    continue
-            cand.append(int(num))
-    print(min(cand) if cand else -1)
+    if n != 1:
+        if t[0] == 0:
+            print(-1)
+            exit()
+        if t[0] == -1:
+            t[0] = 1
+        for i in range(1, n):
+            if t[i] == -1:
+                t[i] = 0
+    else:
+        if t[0] == -1:
+            t[0] = 0
+    print(''.join(map(str, t)))
 
 if __name__ == '__main__':
     main()
