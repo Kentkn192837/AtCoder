601c5757edcca705852fcbb8621130b5eff30892
Kentkn192837 <agriculture5012@gmail.com>
2022-08-16 17:48:28 +0900

    AC

--- Training/Easy/005/main.py
+++ Training/Easy/005/main.py
@@ -1,12 +1,18 @@
 def main():
     N, M, C = map(int, input().split())
     B = list(map(int, input().split()))
-    A = [list(map(int, input().split())) for _ in range(N)]
-    print(f'N: {N}')
-    print(f'M: {M}')
-    print(f'C: {C}')
-    print(*B)
-    print(A)
+    cnt = 0
+
+    for _ in range(N):
+        A = list(map(int, input().split()))
+        result = C
+        for a, b in zip(A, B):
+            result += a * b
+        if result <= 0:
+            continue
+        cnt += 1
+
+    print(cnt)
 
 if __name__ == '__main__':
     main()
