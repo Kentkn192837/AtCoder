264c62a0073e49105fe226dcd9e1a3f8b09db177
Kentkn192837 <agriculture5012@gmail.com>
2023-05-03 01:11:46 +0900

    標準入力を受け取り

--- Training/Easy/010/Main.java
+++ Training/Easy/010/Main.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        final int N = sc.nextInt();
+        final int[] a = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = sc.nextInt();
+        }
+        
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
     }
 }
