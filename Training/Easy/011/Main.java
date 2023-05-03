ea53bc2bde660d62d0470502e965d7e3dba51822
Kentkn192837 <agriculture5012@gmail.com>
2023-05-04 00:02:17 +0900

    first commit

---
+++ Training/Easy/011/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Main {
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        final int N = sc.nextInt();
+        sc.close();
+    }
+}
