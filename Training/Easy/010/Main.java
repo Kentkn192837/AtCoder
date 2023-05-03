6038534304579480d33762e0f6279e80955ed11d
Kentkn192837 <agriculture5012@gmail.com>
2023-05-03 17:09:21 +0900

    AC

--- Training/Easy/010/Main.java
+++ Training/Easy/010/Main.java
@@ -1,17 +1,48 @@
 import java.util.Scanner;
+import java.util.Collections;
+import java.util.ArrayList;
 
-public class Main {
+class Score {
+    final int score;
+
+    Score(final int score) {
+        if (score < 0) {
+            throw new IllegalArgumentException("スコアが負です。");
+        }
+        this.score = score;
+    }
+
+    Score add(final Score otherScore) {
+        final int addedScore = this.score + otherScore.score;
+        return new Score(addedScore);
+    }
+}
+
+
+class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        final Scanner sc = new Scanner(System.in);
         final int N = sc.nextInt();
-        final int[] a = new int[N];
+        final ArrayList<Integer> a = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            a[i] = sc.nextInt();
+            a.add(sc.nextInt());
+        }
+        sc.close();
+
+        Collections.sort(a, Collections.reverseOrder());
+
+        Score alice = new Score(0);
+        Score bob = new Score(0);
+        for (int i = 0; i < a.size(); i++) {
+            final Score getScore = new Score(a.get(i));
+            if (i % 2 == 0) {
+                alice = alice.add(getScore);
+                continue;
+            }
+            bob = bob.add(getScore);
         }
         
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i] + " ");
-        }
-        System.out.println();
+        final int result = Math.abs(alice.score - bob.score);
+        System.out.println(result);
     }
 }
