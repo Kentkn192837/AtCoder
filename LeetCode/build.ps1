a450fcc6b06269926d520d47b178463900ee28f7
Kentkn192837 <agriculture5012@gmail.com>
2026-09-24 01:13:54 +0900

    プログラム実行処理をテンプレート化

--- LeetCode/build.ps1
+++ LeetCode/build.ps1
@@ -4,6 +4,7 @@ param(
 
 mkdir ${dirPath}
 Copy-Item ".\template" "${dirPath}\main.py"
+Copy-Item ".\template.run.ps1" "${dirPath}\run.ps1"
 new-item -Path "${dirPath}\1.txt" -ItemType File
 new-item -Path "${dirPath}\2.txt" -ItemType File
 new-item -Path "${dirPath}\3.txt" -ItemType File
---
+++ LeetCode/template.run.ps1
@@ -0,0 +1,3 @@
+Get-Content ".\1.txt" | python ".\main.py"
+Get-Content ".\2.txt" | python ".\main.py"
+Get-Content ".\3.txt" | python ".\main.py"
