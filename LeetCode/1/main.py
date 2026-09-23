cbfeee5c7b553b70bdbfa4059a032ffeb6669c5e
Kentkn192837 <agriculture5012@gmail.com>
2026-09-24 01:10:23 +0900

    first commit

---
+++ LeetCode/1/main.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        print(nums)
+        print(target)
+
+
+print(Solution().twoSum(list(map(int, input().split(","))), int(input())))
