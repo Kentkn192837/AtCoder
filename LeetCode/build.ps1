fde78b1b80b82a4d0bbfeb5793c8cf1ee2b37e49
Kentkn192837 <agriculture5012@gmail.com>
2026-09-23 15:06:58 +0900

    一度に作れるテストデータファイルの数を3つに変更

--- LeetCode/build.ps1
+++ LeetCode/build.ps1
@@ -2,6 +2,8 @@ param(
     [string]$dirPath
 )
 
-mkdir $dirPath
-Copy-Item .\template $dirPath\main.py
-new-item -Path $dirPath\1.txt -ItemType File
+mkdir ${dirPath}
+Copy-Item ".\template" "${dirPath}\main.py"
+new-item -Path "${dirPath}\1.txt" -ItemType File
+new-item -Path "${dirPath}\2.txt" -ItemType File
+new-item -Path "${dirPath}\3.txt" -ItemType File
