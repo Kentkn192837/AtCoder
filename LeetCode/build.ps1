1e438d09f15557b0f293ddb02f4db56ed5b18841
Kentkn192837 <agriculture5012@gmail.com>
2026-09-27 01:50:57 +0900

    見直し

--- LeetCode/build.ps1
+++ LeetCode/build.ps1
@@ -1,9 +1,15 @@
 param(
-    [string]$dirPath
+    [string]$dirPath,
+    [string]$methodName
 )
 
-mkdir ${dirPath}
-Copy-Item ".\template" "${dirPath}\main.py"
+if (!(Test-Path $dirPath)) {
+    mkdir ${dirPath}
+}
+
+$content = Get-Content ".\template"
+$content = $content -replace "XXXXXXXXXXXXXXX", ${methodName}
+Set-Content "${dirPath}\main.py" ${content}
 Copy-Item ".\template.run.ps1" "${dirPath}\run.ps1"
 new-item -Path "${dirPath}\1.txt" -ItemType File
 new-item -Path "${dirPath}\2.txt" -ItemType File
